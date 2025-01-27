991e0877cfe22eba016335a707f0e77e6413d80e
Vidush142 <varulkumar142@gmail.com>
2025-01-27 16:26:15 +0000

    Modified mini project

--- Python/Mini_Project/etl_project.py
+++ Python/Mini_Project/etl_project.py
@@ -1,7 +1,9 @@
 import csv
 
 def extract_data(file_name):
-    with open('student_test_scores_extended.csv', newline='') as csvfile:
-        csvreader = csv.reader(csvfile, delimiter=' ', qoutechar='|')
+    with open(file_name, newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
         for row in csvreader:
             print(', '.join(row))
+
+extract_data('student_test_scores_extended.csv')
