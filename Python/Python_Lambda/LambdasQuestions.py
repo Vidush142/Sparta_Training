de3df5c5df0a1b74c4d889aede2de1e9b133133f
Vidush142 <varulkumar142@gmail.com>
2025-02-05 12:00:22 +0000

    Completed lambda exercises file

--- Python/Python_Lambda/LambdasQuestions.py
+++ Python/Python_Lambda/LambdasQuestions.py
@@ -19,12 +19,21 @@ def addition(a, b, c):
 
 # A1a:
 
+square_l = lambda x:x*x
 
+percentage_l = lambda x: x/100
+
+multiplier_l = lambda x,y: x*y
+
+addition_l = lambda x,y,z: x+y+z
+
+# print(addition_l(1,2,3))
 print("\nQ1b\n")
 # Q1b: Write an explanation of how this factorial lambda function works
 factorial = lambda a: a*factorial(a-1) if (a>1) else 1
 
 # A1b:
+# It is a recursive function that runs the expression a*factorial(a-1) if the if condition is met else returns 1
 
 
 print("\nQ1c\n")
@@ -33,5 +42,7 @@ print("\nQ1c\n")
 list_of_numbers = [23, 345, 45, 76, 87, 4, 2, 0]
 
 # A1c:
+square_list = list(map(lambda x: x**2, list_of_numbers))
+print(square_list)
 
 # -------------------------------------------------------------------------------------- #
